438385071450266e6eb3b026aa00c53e3e69c60e
urielBarhum <uryel.uryel7890@gmail.com>
2024-02-06 22:11:22 +0200

    its second brach

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
 function chnge(){
     var heading = document.querySelector('h1')
+    var heading2 = document.querySelector('h2')
+    heading2.style.color='green'
     heading.style.color = 'blue'
 }
